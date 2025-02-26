63f3a3bc3b6d5fd8c45f0f90ec2937f25e27023a
Jan Kotrc <john.kotrc@gmail.com>
2025-02-26 01:02:20 +0100

    add lark to pre, customize build script

--- build_and_push.sh
+++ build_and_push.sh
@@ -3,13 +3,13 @@
 # Get Year and Commit Hash
 YEAR=$(date +%Y)
 COMMIT_HASH=$(git rev-parse --short HEAD)
-IMAGE_TAG="bitswan/pipeline-runtime-environment"
+IMAGE_TAG="torrc/pre-lark"
 
 # Build and push Docker images
-docker build -t $IMAGE_TAG:latest -t $IMAGE_TAG:$YEAR-${GITHUB_RUN_ID}-git-$COMMIT_HASH -f ./Dockerfile .
+docker build -t $IMAGE_TAG:latest -t $IMAGE_TAG:$YEAR-git-$COMMIT_HASH -f ./Dockerfile .
 
 docker push $IMAGE_TAG:latest
-docker push $IMAGE_TAG:$YEAR-${GITHUB_RUN_ID}-git-$COMMIT_HASH
+docker push $IMAGE_TAG:$YEAR-git-$COMMIT_HASH
 
 # Push a tag with the image ID
 IMAGE_ID=$(docker images --no-trunc -q $IMAGE_TAG:latest | sed 's/:/_/g')
