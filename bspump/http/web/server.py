0205e7c053c6622c724a2d3a9c27b22f40430834
Jan Kotrc <john.kotrc@gmail.com>
2025-05-14 23:23:47 +0200

    removed parameter that slipped through refactor

--- bspump/http/web/server.py
+++ bspump/http/web/server.py
@@ -182,6 +182,8 @@ class ProtectedWebRouteSource(WebRouteSource):
     """
     Web route source that requires a secret in a qparam or in the BearerToken.
     """
+    def test_secret(self, secret):
+        return secret == self.Config["secret"]
 
     async def handle_request(self, request: Request):
         try:
@@ -197,7 +199,7 @@ class ProtectedWebRouteSource(WebRouteSource):
                 )
                 return await response_future
 
-            return await gate_response(request, self.Config["secret"], response_fn)
+            return await gate_response(request, lambda secret: self.test_secret(secret), response_fn)
         except Exception:
             L.exception("Exception in WebSource")
             return aiohttp.web.Response(status=500)
@@ -243,7 +245,7 @@ class WebFormSource(WebRouteSource):
             self.fields = self.generate_fields(request)
         if request.content_type == "application/json":
             defaults = self.extract_defaults(request)
-            field_info = {f.name: f.get_params(defaults) for f in self.fields}
+            field_info = {f.name: f.get_params() for f in self.fields}
             return aiohttp.web.json_response(field_info)
         return aiohttp.web.Response(
             text=self.render_form(request),
